d3a66b1d1b22d4204e45bb30a1b09290ed65dcbe
Shreshtha Verma <shreshthaverma2915@gmail.com>
2021-04-13 17:02:15 -1200

    First Commit

---
+++ API/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import PlayerSerializer
+from .models import Player
+
+class PlayerViewSet(viewsets.ModelViewSet):
+    queryset = Player.objects.all().order_by('name')
+    serializer_class = PlayerSerializer
+# Create your views here.
